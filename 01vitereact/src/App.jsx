5c0c99e16c24a2560a858464b2e0171a22f4d554
sandhya324 <sdhya3842@gmail.com>
2026-05-29 21:24:58 +0530

    start of learning react in vite

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,10 +2,14 @@ import './App.css'
 import Learn from './learn.jsx'
 
 function App() {
+
+  const username = "John Doe"
   
   return (
     <> // React Fragement
       <Learn/>
+      <h4>Learning react to build modern web applications ! {username} </h4> //{username} this is called expression , evaluated expression means final outcome.
+      <p>test paragraph</p>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Visit google</a>
+)
+
+
+
+
+
+
+const anotherUser = "learning react is fun  !"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com'},
+  'Click me to visit google',
+  anotherUser
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  reactElement
 )
