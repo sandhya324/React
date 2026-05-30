41b085ef6dfcfc0b92bf311ceb213ceb1e4ce520
sandhya324 <sdhya3842@gmail.com>
2026-05-30 17:41:01 +0530

    chapter 4

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+  
+  // let counter = 55
+
+  const addValue = () =>{
+    console.log("clicked", {counter});
+    // counter += 1;
+    if(counter < 20){
+      setCounter(counter + 1);
+    }
+  }
+
+  
+  const removeValue = () => {
+    console.log("clicked", {counter});
+    // counter -= 1;
+    if(counter>0){
+      setCounter(counter - 1)
+    }
+
+  }
+   
+  return (
+    <>
+       <h1>Learning React</h1>
+       <h2>Counter value: {counter}</h2>
+
+       <button  onClick={addValue}>Add value : {counter}</button> // 0 to 20 
+       <br />
+       <button onClick={removeValue} >remove value : {counter}</button>
+    </>
+  )
+}
+
+export default App
