3ba6b7140a1f0ab96847d9a690321d65c881b35c
sandhya324 <sdhya3842@gmail.com>
2026-06-03 18:56:02 +0530

    design Home page

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import contact from './components/contact'
+function App() {
+  return (
+    <>
+    <h1 className="text-6xl font-bold text-red-500">
+      Hello Tailwind
+    </h1>
+
+    <contact channel="tailwind"/>
+    </>
+  )
+}
+
+export default App
