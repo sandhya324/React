529aeb4b257ab6743cb0ad404396060822dce13e
sandhya324 <sdhya3842@gmail.com>
2026-06-04 15:00:32 +0530

    Learnt about onclick

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const App = () => {
+function App() {
+  const [color, setColor] = useState("olive")
+
   return (
-    <div>
-      <h1>Hello, React!</h1>
+
+    <div
+      className="w-full h-screen"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button onClick={() => setColor("red")}  className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{background: "red"}}>Red</button>
+          <button onClick={() => setColor("blue")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{background: "blue"}}>Blue</button>
+          <button onClick={() => setColor("olive")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{background: "olive"}}>Olive</button>
+          <button onClick={() => setColor("green")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{background: "green"}}>Green</button>
+        </div>
+      </div>
+
+
+
     </div>
   )
 }
