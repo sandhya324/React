42043a5b3a1e24ab959444168edd4661f13bd874
sandhya324 <sdhya3842@gmail.com>
2026-06-05 08:34:46 +0530

    passward

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>
+
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+       </h1>
       
     </div>
   )
