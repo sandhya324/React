867dd12692e701864eba1f13abb17dd84627bb89
sandhya324 <sdhya3842@gmail.com>
2026-06-17 19:44:14 +0530

    added footer

--- 07reactRouter/src/components/Header/Header.jsx
+++ 07reactRouter/src/components/Header/Header.jsx
@@ -43,7 +43,6 @@ export default function Header() {
                                 </NavLink>
                             </li>
                             
-                            
                         </ul>
                     </div>
                 </div>
