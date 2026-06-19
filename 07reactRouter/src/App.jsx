41d043de22aa834e80b074d6d8c653f621b45e29
sandhya324 <sdhya3842@gmail.com>
2026-06-19 17:58:06 +0530

    can't able to complete it will try after a time

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import Home from './components/Home/Home'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <>
-       <Header />
-       <Home />
-       <Footer />
     </>
   )
 }
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import Layout from './components/Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+   <React.StrictMode>
+    <RouterProvider router={router} /> 
+   </React.StrictMode>,
 );
